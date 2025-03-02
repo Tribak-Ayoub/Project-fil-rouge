76d31d0de037b5bf7ece1c5aa01c3a02f53cae34
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-03-02 23:10:07 +0000

    save

--- Technical Branch/Technical Analysis/Multi-Language/MultiLangApp/routes/web.php
+++ Technical Branch/Technical Analysis/Multi-Language/MultiLangApp/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('lang/{locale}', function ($locale) {
+    if (in_array($locale, ['en', 'fr'])) {
+        Session::put('locale', $locale);
+    }
+    return Redirect::back();
+})->name('switch.lang');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
