b8fceaabeb44fb409ed8461f509e829bf0cab3ec
Ayoub Tribak <tribak.elyedri.ayoub@gmail.com>
2025-05-19 10:17:46 +0100

    Revert "Pkg core"

--- Sanction-Manager-App/modules/PkgSanction/Routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/dashboard', function () {
-    return Inertia::render('PkgSanction::Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
